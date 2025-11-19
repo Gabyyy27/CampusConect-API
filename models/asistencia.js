b0ee88f33cb4b246f0c66d36f9b8f78423f09c31
Gabyyy27 <lgaby6944@gmail.com>
2025-11-19 17:06:53 -0600

    Endpoint crear nuevos usuarios

--- models/asistencia.js
+++ models/asistencia.js
@@ -4,18 +4,13 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Asistencia extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+  
     static associate(models) {
   // Una Asistencia pertenece a un Usuario
-  this.belongsTo(models.Usuario, {
+  this.belongsTo(models.Usuarios, {
     foreignKey: 'usuarioId',
     as: 'usuario'
   });
-  
   // Una Asistencia pertenece a un Evento
   this.belongsTo(models.Evento, {
     foreignKey: 'eventoId',
